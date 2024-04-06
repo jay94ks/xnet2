6c4a1e4feb61ce63e45371648cb41210f464fae7
Jay <33371449+jay94ks@users.noreply.github.com>
2024-04-06 18:17:11 +0900

    Added default `IRequest` and `IResponse` classes.

---
+++ Xnet/Packets/RequestBase.cs
@@ -0,0 +1,20 @@
+﻿namespace Xnet.Packets
+{
+    /// <summary>
+    /// Base class for request packets.
+    /// </summary>
+    public abstract class RequestBase : IRequest
+    {
+        /// <inheritdoc/>
+        public Guid RequestId { get; set; }
+
+        /// <inheritdoc/>
+        public abstract void Encode(BinaryWriter Writer);
+
+        /// <inheritdoc/>
+        public abstract void Decode(BinaryReader Reader);
+
+        /// <inheritdoc/>
+        public abstract Task<IResponse> HandleAsync(Connection Conn);
+    }
+}
---
+++ Xnet/Packets/ResponseBase.cs
@@ -0,0 +1,18 @@
+﻿namespace Xnet.Packets
+{
+    /// <summary>
+    /// Base class for response packets.
+    /// </summary>
+    public abstract class ResponseBase : IResponse
+    {
+        /// <inheritdoc/>
+        public Guid RequestId { get; set; }
+
+        /// <inheritdoc/>
+        public abstract void Encode(BinaryWriter Writer);
+
+        /// <inheritdoc/>
+        public abstract void Decode(BinaryReader Reader);
+
+    }
+}
